f0be90041fdb68ff455293288ee0ae1bf6e85c57
hp <lars75@inbox.ru>
2022-11-09 18:52:38 +0300

    2. Каркас шахматной доски [#792 #359415]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -2,6 +2,7 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
+
 import java.util.Arrays;
 
 public final class Logic {
@@ -23,7 +24,7 @@ public final class Logic {
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Figure figure : figures) {
             for (Cell cell : steps) {
-                if(figure.position().equals(cell)) {
+                if (figure.position().equals(cell)) {
                     throw new OccupiedCellException("Cell full figure");
                 }
             }
--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -2,6 +2,7 @@ package ru.job4j.chess.firuges.black;
 
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
+
 import static org.assertj.core.api.Assertions.*;
 
 class BishopBlackTest {
@@ -22,7 +23,7 @@ class BishopBlackTest {
     @Test
     void way() {
         BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        Cell[] expected = new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Cell[] expected = new Cell[]{Cell.D2, Cell.E3, Cell.F4, Cell.G5};
         Cell[] out = bishopBlack.way(Cell.G5);
         assertThat(out).isEqualTo(expected);
     }
